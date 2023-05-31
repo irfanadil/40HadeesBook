81fbb562beecb3f75d1a42d31665f0ae9937180d
Muhammad Irfan Adil <itsmeadil2@hotmail.com>
2023-05-31 18:04:45 +0300

    text issues fixed
    text issues fixed

--- app/src/main/java/muhammad/fourty/saying/util/StringPatternMatching.kt
+++ app/src/main/java/muhammad/fourty/saying/util/StringPatternMatching.kt
@@ -2,10 +2,12 @@ package muhammad.fourty.saying.util
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Typeface
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
 import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import muhammad.fourty.saying.R
 
 
@@ -33,6 +35,13 @@ object StringPatternMatching {
         )
 
         wordToSpan.setSpan(
+            StyleSpan(Typeface.BOLD),
+            quoteIndex[0],
+            quoteIndex[1],
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        wordToSpan.setSpan(
             RelativeSizeSpan(.1f),
             quoteIndex[0],
             quoteIndex[0]+5,
@@ -50,29 +59,51 @@ object StringPatternMatching {
     }
 
 
+    fun returnHighLightHadeesNumber(hadeesText:Spannable , context:Context) : Spannable {
+
+        val wordToSpan = hadeesText
+        val sentenceExtracted = Regex("Hadees.No.").findAll(wordToSpan)
+            .map { it.range.first }
+            .toMutableList()
+
+       if(sentenceExtracted.size == 1){
+           wordToSpan.setSpan(
+               ForegroundColorSpan(context.resources.getColor(R.color.Red, context.theme)),
+               sentenceExtracted[0],
+               sentenceExtracted[0]+12,
+               Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+           )
+       }
+
+        return wordToSpan
+    }
+
+
+
     fun returnAllMatchingNameOfProphet(prophetSaying:String , context:Context) : Spannable {
 
         val wordToSpan: Spannable = SpannableString(prophetSaying)
-        val pattern1 = "حضور اکرم صلى الله عليه وسلم"
-        val pattern2 = "حضرت محمد صلى الله عليه وسلم"
-        val pattern3 = "محمد صلى الله عليه وسلم"
+        //val pattern1 = "حضور اکرم صلى الله عليه وسلم"
+        //val pattern2 = "حضرت محمد صلى الله عليه وسلم"
+        //val pattern3 = "محمد صلى الله عليه وسلم"
         val pattern4 = "صلى الله عليه وسلم"
-        val pattern5 = "حضوراکرم ﷺ"
-        val pattern6 = "رسول اکرم ﷺ"
-        val pattern7 = "آنحضرت ﷺ"
-        val pattern8 = "آنحضرت صلى الله عليه وسلم"
+        //val pattern5 = "ﷺ"
+        //val pattern5 = "حضوراکرم ﷺ"
+        //val pattern6 = "رسول اکرم ﷺ"
+        //val pattern7 = "آنحضرت ﷺ"
+        //val pattern8 = "آنحضرت صلى الله عليه وسلم"
 
 
 
         val nameList:MutableList<Int> = mutableListOf()
-        nameList.addAll(returnMatchingIndexes(pattern1, prophetSaying))
-        nameList.addAll(returnMatchingIndexes(pattern2, prophetSaying))
-        nameList.addAll(returnMatchingIndexes(pattern3, prophetSaying))
+        //nameList.addAll(returnMatchingIndexes(pattern1, prophetSaying))
+       // nameList.addAll(returnMatchingIndexes(pattern2, prophetSaying))
+        //nameList.addAll(returnMatchingIndexes(pattern3, prophetSaying))
         nameList.addAll(returnMatchingIndexes(pattern4, prophetSaying))
-        nameList.addAll(returnMatchingIndexes(pattern5, prophetSaying))
-        nameList.addAll(returnMatchingIndexes(pattern6, prophetSaying))
-        nameList.addAll(returnMatchingIndexes(pattern7, prophetSaying))
-        nameList.addAll(returnMatchingIndexes(pattern8, prophetSaying))
+        //nameList.addAll(returnMatchingIndexes(pattern5, prophetSaying))
+        //nameList.addAll(returnMatchingIndexes(pattern6, prophetSaying))
+       // nameList.addAll(returnMatchingIndexes(pattern7, prophetSaying))
+       // nameList.addAll(returnMatchingIndexes(pattern8, prophetSaying))
         return highLightProphetName(wordToSpan, nameList , context)
 
     }
